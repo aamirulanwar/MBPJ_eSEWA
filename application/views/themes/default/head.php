b60ad03ca45674f9ac5cbcded1805cc4215b48bb
alwinarul <alwin_arul@yahoo.com>
2020-04-19 02:18:27 +0800

    update laporan hasil perjanjian sewa

--- application/views/themes/default/head.php
+++ application/views/themes/default/head.php
@@ -611,6 +611,10 @@
                             <a class="nav-link <?php echo set_active_menu(uri_segment(2),array('perjanjian'))?>" href="/report/perjanjian_kutipan">
                                 <i class="nav-icon fa fa-circle fa-sm"></i> Laporan Perbandingan</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link <?php echo set_active_menu(uri_segment(2),array('hasil'))?>" href="/report/hasil">
+                                <i class="nav-icon fa fa-circle fa-sm"></i> Laporan Hasil Perjanjian Sewa</a>
+                        </li>
                     </ul>
                 </li>
                 <?php endif;?>
